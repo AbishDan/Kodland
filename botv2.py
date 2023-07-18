673304f82175bef8278b347c24cc62abbc9f5e22
AbishDan <136463402+AbishDan@users.noreply.github.com>
2023-07-18 14:17:36 +0600

    Update botv2.py

--- botv2.py
+++ botv2.py
@@ -1,125 +1,46 @@
-# This example requires the 'members' privileged intent to use the Member converter.
-# This example also requires the 'message_content' privileged intent to function.
-
-import traceback
-import typing
-
 import discord
 from discord.ext import commands
-
+from bot_logic import gen_pass, gen_image,get_duck_image_url,get_fox_image_url
 intents = discord.Intents.default()
-intents.members = True
 intents.message_content = True
 
-bot = commands.Bot('!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def make_a_password(ctx,length):
+    await ctx.send(gen_pass(int(length)))
+@bot.command()
+async def mem(ctx):
+    with open(f'images/{gen_image()}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
 
 
 @bot.command()
-async def userinfo(ctx: commands.Context, user: discord.User):
-    # In the command signature above, you can see that the `user`
-    # parameter is typehinted to `discord.User`. This means that
-    # during command invocation we will attempt to convert
-    # the value passed as `user` to a `discord.User` instance.
-    # The documentation notes what can be converted, in the case of `discord.User`
-    # you pass an ID, mention or username (discrim optional)
-    # E.g. 80088516616269824, @Danny or Danny#0007
-
-    # NOTE: typehinting acts as a converter within the `commands` framework only.
-    # In standard Python, it is use for documentation and IDE assistance purposes.
-
-    # If the conversion is successful, we will have a `discord.User` instance
-    # and can do the following:
-    user_id = user.id
-    username = user.name
-    avatar = user.display_avatar.url
-    await ctx.send(f'User found: {user_id} -- {username}\n{avatar}')
-
-
-@userinfo.error
-async def userinfo_error(ctx: commands.Context, error: commands.CommandError):
-    # If the conversion above fails for any reason, it will raise `commands.BadArgument`
-    # so we handle this in this error handler:
-    if isinstance(error, commands.BadArgument):
-        return await ctx.send('Couldn\'t find that user.')
-    # The default `on_command_error` will ignore errors from this command
-    # because we made our own command-specific error handler,
-    # so we need to log tracebacks ourselves.
-    else:
-        traceback.print_exception(type(error), error, error.__traceback__)
-
-
-# Custom Converter here
-class ChannelOrMemberConverter(commands.Converter):
-    async def convert(self, ctx: commands.Context, argument: str):
-        # In this example we have made a custom converter.
-        # This checks if an input is convertible to a
-        # `discord.Member` or `discord.TextChannel` instance from the
-        # input the user has given us using the pre-existing converters
-        # that the library provides.
-
-        member_converter = commands.MemberConverter()
-        try:
-            # Try and convert to a Member instance.
-            # If this fails, then an exception is raised.
-            # Otherwise, we just return the converted member value.
-            member = await member_converter.convert(ctx, argument)
-        except commands.MemberNotFound:
-            pass
-        else:
-            return member
-
-        # Do the same for TextChannel...
-        textchannel_converter = commands.TextChannelConverter()
-        try:
-            channel = await textchannel_converter.convert(ctx, argument)
-        except commands.ChannelNotFound:
-            pass
-        else:
-            return channel
-
-        # If the value could not be converted we can raise an error
-        # so our error handlers can deal with it in one place.
-        # The error has to be CommandError derived, so BadArgument works fine here.
-        raise commands.BadArgument(f'No Member or TextChannel could be converted from "{argument}"')
-
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
 
 @bot.command()
-async def notify(ctx: commands.Context, target: ChannelOrMemberConverter):
-    # This command signature utilises the custom converter written above
-    # What will happen during command invocation is that the `target` above will be passed to
-    # the `argument` parameter of the `ChannelOrMemberConverter.convert` method and
-    # the conversion will go through the process defined there.
+async def fox(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
+bot.run("MTEyMDY5MjI3MDQ2NzkxNTgxNg.G8QExd.EsnfUCWweZLnBDZhYVi7-20RdRwMNn-YpZAP8g")
 
-    await target.send(f'Hello, {target.name}!')
-
-
-@bot.command()
-async def ignore(ctx: commands.Context, target: typing.Union[discord.Member, discord.TextChannel]):
-    # This command signature utilises the `typing.Union` typehint.
-    # The `commands` framework attempts a conversion of each type in this Union *in order*.
-    # So, it will attempt to convert whatever is passed to `target` to a `discord.Member` instance.
-    # If that fails, it will attempt to convert it to a `discord.TextChannel` instance.
-    # See: https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html#typing-union
-    # NOTE: If a Union typehint converter fails it will raise `commands.BadUnionArgument`
-    # instead of `commands.BadArgument`.
-
-    # To check the resulting type, `isinstance` is used
-    if isinstance(target, discord.Member):
-        await ctx.send(f'Member found: {target.mention}, adding them to the ignore list.')
-    elif isinstance(target, discord.TextChannel):  # this could be an `else` but for completeness' sake.
-        await ctx.send(f'Channel found: {target.mention}, adding it to the ignore list.')
-
-
-# Built-in type converters.
-@bot.command()
-async def multiply(ctx: commands.Context, number: int, maybe: bool):
-    # We want an `int` and a `bool` parameter here.
-    # `bool` is a slightly special case, as shown here:
-    # See: https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html#bool
-
-    if maybe is True:
-        return await ctx.send(number * 2)
-    await ctx.send(number * 5)
-
-
-bot.run('token')
